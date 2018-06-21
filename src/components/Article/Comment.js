d231143956e0d31384de38c672ed7f8fbe8546bb
Will Klein <will.klein@gmail.com>
2018-06-21 00:29:06 -0600

    Add a unit test

--- src/components/Article/Comment.js
+++ src/components/Article/Comment.js
@@ -1,32 +1,29 @@
-import DeleteButton from './DeleteButton';
-import { Link } from 'react-router-dom';
 import React from 'react';
 
 const Comment = props => {
   const comment = props.comment;
-  const show = props.currentUser &&
-    props.currentUser.username === comment.author.username;
+
   return (
-    <div className="card">
+    <div className="card" data-testid="comment">
       <div className="card-block">
         <p className="card-text">{comment.body}</p>
       </div>
       <div className="card-footer">
-        <Link
-          to={`/@${comment.author.username}`}
-          className="comment-author">
-          <img src={comment.author.image} className="comment-author-img" alt={comment.author.username} />
-        </Link>
+        <span className="comment-author">
+          <img
+            alt={comment.author.username}
+            data-testid="comment-author-img"
+            className="comment-author-img"
+            src={comment.author.image}
+          />
+        </span>
         &nbsp;
-        <Link
-          to={`/@${comment.author.username}`}
-          className="comment-author">
+        <span className="comment-author">
           {comment.author.username}
-        </Link>
+        </span>
         <span className="date-posted">
           {new Date(comment.createdAt).toDateString()}
         </span>
-        <DeleteButton show={show} slug={props.slug} commentId={comment.id} />
       </div>
     </div>
   );
